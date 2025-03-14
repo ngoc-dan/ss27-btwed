973e4b6356b3fff27acea52dcd49743979a2362a
ngoc-dan <ngocdan191026@gmail.com>
2025-03-14 15:45:30 +0700

    Update ss27-bai9.js

--- ss27-bai9.js
+++ ss27-bai9.js
@@ -1,5 +1,5 @@
 let array = [5, 8, 6, 3, 4, 2, 9]
-function sortEvenNumbers(arr) {// Lọc và sắp xếp các số chẵn
+function sortEvenNumbers(arr) {
     let evenNumbers = arr.filter(num => num % 2 === 0).sort((a, b) => a - b);
     return arr.map(num => (num % 2 === 0 ? evenNumbers.shift() : num));
 }
